450678ea4bb55e12ab7daf8da3375d2b279ba29b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-07 16:30:46 -0400

    Destructures the 'math' constant to just import the 'add' function so we don't have to write the 'math.add()' every time in each assertion

--- math/math.spec.js
+++ math/math.spec.js
@@ -1,4 +1,4 @@
-const math = require('./math.js');
+const { add } = require('./math.js');
 
 // SUT: System Under Test
 // test suite > test cases > assertions > matchers
@@ -11,16 +11,21 @@ const math = require('./math.js');
 // Real-World-Scenario
 // adds two numbers
 test('adds two numbers correctly', () => {
-    expect(math.add(2, 2)).toBe(4); 
-    expect(math.add(1, 3)).toBe(4); 
-    expect(math.add(-1, 3)).toBe(2); 
+    expect(add(2, 2)).toBe(4); 
+    expect(add(1, 3)).toBe(4); 
+    expect(add(-1, 3)).toBe(2); 
 });
 
 // a test case that expects to THROW an error when passed a string
 it('math.add() should return null when arguments are not numbers', () => {
-    expect(() => {
-        math.add('3', 2);
-      }).toThrow();
+    expect(add()).toBeNull();
+    expect(add(1)).toBeNull();
+    expect(add(null, 1)).toBeNull();
+    expect(add(undefined, 1)).toBeNull();
+    expect(add(3, '4')).toBeNull();
+    expect(add({}, '4')).toBeNull();
+    expect(add([], '4')).toBeNull();
+    expect(add(NaN, '4')).toBeNull();
 });
 
 //---------------------------------------------
@@ -37,18 +42,18 @@ it('math.add() should return null when arguments are not numbers', () => {
 
 //---------------------------------------------
 // Perfect-World-Scenario
-test('adds two numbers', () => {
-    // Phase 1: Arrange (setup)
-    const expected = 4;
+// test('adds two numbers', () => {
+//     // Phase 1: Arrange (setup)
+//     const expected = 4;
 
-    // Phase 2: Act (run SUT)
-    const actual = math.add(2, 2);
+//     // Phase 2: Act (run SUT)
+//     const actual = math.add(2, 2);
 
-    // Phase 3: assert
-    expect(actual).toBe(expected); // assertion
-    // using the '.toBe()' matcher
+//     // Phase 3: assert
+//     expect(actual).toBe(expected); // assertion
+//     // using the '.toBe()' matcher
 
-    // shorter way to consolidate all three phases 
-    // into one line (below)
-    expect(math.add(2, 2)).toBe(4); 
-});
+//     // shorter way to consolidate all three phases 
+//     // into one line (below)
+//     expect(math.add(2, 2)).toBe(4); 
+// });
