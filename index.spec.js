66c9a4c210379916a271b525e0f8e3ce8fd0b686
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-07 17:20:14 -0400

    Adds commented out code that lists some other interesting matchers from jestjs.io

--- index.spec.js
+++ index.spec.js
@@ -1,4 +1,15 @@
 it('checks equality', () => {
     expect({ one: 1 }).toEqual({ one: 1 });
-    expect([1, 2]).toEqual([1, 2]);
+    expect([1, 2, 3, 4]).toEqual(expect.arrayContaining([1, 2, 3]));
 });
+
+/*
+other interesting matchers
+- .not() => tests the OPPOSITE of the passed in test
+- .toBeCloseTo() => checks equality of floating point numbers; (number, numDigits)
+- .toMatch() => checks that a string matches a reg ex
+- .toHaveLength() => checks that a object has a .length property and that it is set to a numeric value
+- .arrayContaining() => matches a received array which contains all of the elements in the expected array
+- .stringContaining() => matches the received value if it is a string that contains the exact expected string
+- .toContain() => checks that an item (or substring) is inside of an array (or another string)
+*/
